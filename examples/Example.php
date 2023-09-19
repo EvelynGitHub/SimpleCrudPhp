9ec903001f578f1d319f66426a992595eca6c569
Rodrigo Yuri <rodrigo_yuri@hotmail.com>
2023-09-19 12:03:07 -0300

    refactor: melhorias estrutura

--- examples/Example.php
+++ examples/Example.php
@@ -2,77 +2,70 @@
 
 use SimplePhp\SimpleCrud\Crud;
 
-class Example
+class Example extends Crud
 {
-    private Crud $crud;
+  public function insertExample(array $data = null)
+  {
+    $query = $this->insert("example", $data)->execute();
 
-    public function __construct(Crud $crud)
-    {
-        $this->crud = $crud;
+    if ($query) {
+      return "example cadastrado";
+    } else {
+      return $this->getError();
     }
+  }
 
-    public function insertExample(array $data = null)
-    {
-        $query = $this->crud->insert("example", $data)->execute();
+  public function showExample(int $id)
+  {
+    $query = $this->select("qt_example as qt, nm_example as nome")
+      ->from("example")
+      ->where("cd_example = ?", [$id])
+      ->execute("fetchAll");
 
-        if ($query) {
-            return "example cadastrado";
-        } else {
-            return $this->crud->getError();
-        }
+    return $query;
+  }
+
+  public function updateExample(int $id, array $data)
+  {
+    $crud = $this->update("example", "nm_example = ?, dt_example = ?", $data)
+      ->where("cd_example = ?", [$id])->execute();
+
+    if ($crud) {
+      return "Atualizado Com Sucesso";
+    } else {
+      return $this->getError();
     }
+  }
 
-    public function showExample(int $id)
-    {
-        $query = $this->crud->select("qt_example as qt, nm_example as nome")
-            ->from("example")
-            ->where("cd_example = ?", [$id])
-            ->execute("fetchAll");
+  public function deleteExample($id)
+  {
+    $crud = $this->delete()->from("example")->where("cd_example = ?", [$id])->execute();
 
-        return $query;
-    }
+    return $crud;
+  }
 
-    public function updateExample(int $id, array $data)
-    {
-        $crud = $this->crud->update("example", "nm_example = ?, dt_example = ?", $data)
-            ->where("cd_example = ?", [$id])->execute();
+  public function filterExample($data = array())
+  {
 
-        if ($crud) {
-            return "Atualizado Com Sucesso";
-        } else {
-            return $this->crud->getError();
-        }
-    }
+    $queryFilter = $this->select("cd_example, DATE_FORMAT(dt_example, '%d/%m/%Y') as dt_example")
+      ->from("example");
 
-    public function deleteExample($id)
-    {
-        $crud = $this->crud->delete()->from("example")->where("cd_example = ?", [$id])->execute();
-
-        return $crud;
-    }
-
-    public function filterExample($data = array())
-    {
-
-        $queryFilter = $this->crud->select("cd_example, DATE_FORMAT(dt_example, '%d/%m/%Y') as dt_example")
-            ->from("example");
-
-        $queryFilter = $queryFilter
-            ->where("nm_example LIKE (?) OR nm_example2 LIKE (?)", ["%exam%", "%{$data["search"]}%"]);
+    $queryFilter = $queryFilter
+      ->where("nm_example LIKE (?) OR nm_example2 LIKE (?)", ["%exam%", "%{$data["search"]}%"]);
 
 
-        $totalRegisterInQuery = $queryFilter->execute("rowCount", false);
+    $totalRegisterInQuery = $queryFilter->execute("rowCount", false);
 
-        $query = $queryFilter
-            ->order("dt_example", "DESC")
-            ->limit(0, 10)
-            ->execute("fetchAll");
+    $query = $queryFilter
+      ->order("dt_example", "DESC")
+      ->limit(0, 10)
+      ->execute("fetchAll");
 
-        $totalRegisterInTable = $this->crud->select("COUNT(*) as count")
-            ->from("example")
-            ->execute("fetch")
-            ->count;
+    $totalRegisterInTable = $this->select("COUNT(*) as count")
+      ->from("example")
+      ->execute("fetch")
+      ->count;
 
-        return [$totalRegisterInQuery, $query, $totalRegisterInTable];
-    }
+    return [$totalRegisterInQuery, $query, $totalRegisterInTable];
+  }
 }
--- src/Connection.php
+++ src/Connection.php
@@ -2,41 +2,40 @@
 
 namespace SimplePhp\SimpleCrud;
 
-use Exception;
 use PDO;
 use PDOException;
 
 class Connection
 {
-    private static $instance;
-    private static $error;
+  private static $instance;
+  private static $error;
 
-    public static function getInstance()
-    {
-        if (empty(self::$instance)) {
-            try {
+  public static function getInstance()
+  {
+    if (empty(self::$instance)) {
+      try {
 
-                self::$instance = new PDO(
-                    DATABASE["driver"] . ":host=" . DATABASE["host"] . ";dbname=" . DATABASE["dbname"] . ";port=" . DATABASE["port"],
-                    DATABASE["username"],
-                    DATABASE["passwd"],
-                    DATABASE["options"]
-                );
-            } catch (PDOException $exception) {
-                self::$error = $exception;
+        self::$instance = new PDO(
+          DATABASE["driver"] . ":host=" . DATABASE["host"] . ";dbname=" . DATABASE["dbname"] . ";port=" . DATABASE["port"],
+          DATABASE["username"],
+          DATABASE["passwd"],
+          DATABASE["options"]
+        );
+      } catch (PDOException $exception) {
+        self::$error = $exception;
 
-                throw new ConnectionException(
-                    $exception->getMessage(),
-                    $exception->getCode()
-                );
-            }
-        }
-
-        return self::$instance;
+        throw new ConnectionException(
+          $exception->getMessage(),
+          $exception->getCode()
+        );
+      }
     }
 
-    public static function getError()
-    {
-        return self::$error;
-    }
+    return self::$instance;
+  }
+
+  public static function getError()
+  {
+    return self::$error;
+  }
 }
