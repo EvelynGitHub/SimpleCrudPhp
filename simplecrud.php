8e5036b2049dc724ed00ed9aba20125368254a24
Evelyn <evelynbrandao15@gmail.com>
2025-08-20 17:22:28 -0300

    feat: Criando aquivo de configuração para execuções de comandos

---
+++ simplecrud.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'migrations' => [
+        'path' => __DIR__ . '/examples/migrations',
+        'extension' => 'sql',
+    ],
+    'seeds' => [
+        'path' => __DIR__ . '/examples/seeds',
+        'extension' => 'php',
+    ],
+];
